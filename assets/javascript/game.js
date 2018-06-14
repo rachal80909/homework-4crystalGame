26c97b9261a35b1541756943bda1f44dd7dff567
Rachal Metz <[rachal80909@yahoo.com]>
2018-06-14 17:51:43 -0600

    crystal game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,62 @@
-var targetNumber = Math.floor(Math.random() * 99) + 1;
+// Institute Variables
 
-$("#number-to-guess").text(targetNumber);
-
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
 var counter = 0;
+var images = ["./assets/blue.jpeg", "./assets/ruby.png", "./assets/green.jpeg", "./assets/yellow.jpeg"];
 
-// Now for the hard part. Creating multiple crystals each with their own unique number value.
+// Functions
 
-// We begin by expanding our array to include four options.
-var numberOptions = [10, 5, 3, 7];
-
-// Next we create a for loop to create crystals for every numberOption.
-for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+function randomTargetNumber() {
+    targetNumber = Math.floor(Math.random() * 102) + 19;
 }
 
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function() {
-
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
-
-    if (counter === targetNumber) {
-        alert("You win!");
-    } else if (counter >= targetNumber) {
-        alert("You lose!!");
+function resetCrystals() {
+    for (var i = 0; i < images.length; i++) {
+        var crystal = $("<img>");
+        crystal.addClass("crystal");
+        crystal.attr("src", images[i]);
+        crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+        crystal.attr("height", "100");
+        $(".crystal-images").append(crystal);
     }
+}
 
-});
+function resetHTML() {
+    $(".target-number").html(targetNumber);
+    $(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+    $(".score-number").html(counter);
+    $(".crystal-images").empty();
+}
+
+function totalReset() {
+    randomTargetNumber();
+    counter = 0;
+    resetHTML();
+    resetCrystals();
+}
+
+// Running Code
+
+// Inital Page Set Up
+randomTargetNumber();
+resetHTML();
+resetCrystals();
+
+// Click Functions
+function crystalClick() {
+    //attr returns first value of selected html element
+    counter += parseInt($(this).attr("value"));
+    $(".score-number").html(counter);
+    if (counter == targetNumber) {
+        wins++;
+        totalReset();
+    } else if (counter > targetNumber) {
+        losses++;
+        totalReset();
+    };
+};
+
+//Throughout life cycle of the document, accounting for every single time document is dynamically changed execute crystalClick function
+$(document).on("click", ".crystal", crystalClick);
